6831e7dd21b0186427f587b3bbf66042ae0bdd85
Deepti1909 <deepti.jinu@gmail.com>
2020-05-14 12:24:55 -0400

    second commit by Dev2

--- src/main/java/Cucumber/Automation/App.java
+++ src/main/java/Cucumber/Automation/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Working on the first CR");
+        System.out.println( "First update was a success");
+        
     }
 }
