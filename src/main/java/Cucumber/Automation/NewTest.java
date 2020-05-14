4c2cf09b872457e1481f6a0ab8a74444ec24dca3
Deepti1909 <deepti.jinu@gmail.com>
2020-05-14 13:15:38 -0400

    Dev1 Architect commit

---
+++ src/main/java/Cucumber/Automation/NewTest.java
@@ -0,0 +1,11 @@
+package Cucumber.Automation;
+
+public class NewTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Branch modification was a success"); //--branch code from Dev2
+	}
+
+}
